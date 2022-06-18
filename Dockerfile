062fbfb8351120057f5c8ae8f7e40c64c112e1cc
Dago Romer <dagoromer85@gmail.com>
2022-06-18 23:29:20 +0200

    stuff

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,8 @@ RUN pip install -r requirements.txt
 RUN pip install --force-reinstall MarkupSafe==2.0.1
 
 COPY src /src
-WORKDIR /root/.ssh
+WORKDIR /src
 
-CMD [ "tail" , "-f", "/dev/null"]
+RUN chmod +x /src/init.sh
+# CMD ["/bin/sh", "/src/init.sh"]
+# CMD [ "tail" , "-f", "/dev/null"]
--- src/init.sh
+++ src/init.sh
@@ -4,4 +4,6 @@ cp /root/ssh-keys/ssh-privatekey /root/.ssh/id_rsa
 cp /root/ssh-keys/ssh-publickey /root/.ssh/id_rsa.pub
 echo "CLONING REPO"
 git clone $(cat /root/dbt-config/$PROJECT_NAME) /src/dbt
+echo "RUNNING DBT"
+dbt run -t dev-password --project-dir /src/dbt/$PROJECT_NAME/ --profiles-dir /src/dbt/dbt_profiles/
 echo "DONE HERE"
