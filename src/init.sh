986c38b593d9eaeb6561205ea627b3c5737ae84b
Dago Romer <dagoromer85@gmail.com>
2022-06-21 15:14:56 +0200

    run dbt build

--- src/init.sh
+++ src/init.sh
@@ -4,10 +4,10 @@ cp /root/ssh-keys/ssh-privatekey /root/.ssh/id_ed25519
 cp /root/ssh-keys/ssh-publickey /root/.ssh/id_ed25519.pub
 echo "CLONING REPO"
 git clone $(cat /root/dbt-config/$PROJECT_NAME) /src/dbt
-echo "CHECKS"
-ls /src/dbt
-cat /src/dbt/dbt_profiles/profiles.yml
 echo "RUNNING DBT"
+echo "BUILD"
+dbt build -t dev-password --project-dir /src/dbt/$PROJECT_NAME/ --profiles-dir /src/dbt/dbt_profiles/
+echo "RUN"
 dbt run -t dev-password --project-dir /src/dbt/$PROJECT_NAME/ --profiles-dir /src/dbt/dbt_profiles/
 echo "DONE WITH DBT"
 echo "POST-RUNNING DBT"
